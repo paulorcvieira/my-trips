14c62f892041913f17d1e2c4f96276e3f72af147
Paulo Vieira <paullocostta86@gmail.com>
2021-03-07 10:53:20 -0300

    :sparkles: ui customization and responsiveness bug fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -43,7 +43,7 @@ export default function Home({ places, toggleTheme }: Props) {
         <S.IconInfo aria-label="About" />
       </LinkWrapper>
 
-      <Map places={places} toggleTheme={toggleTheme} />
+      <Map places={places} />
     </>
   )
 }
